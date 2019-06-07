bf2683bca0e2f4a4af210f3aaf36a544907defc9
Malik Piara <malikpiara@gmail.com>
2019-06-07 10:40:59 +0100

    connected react app to airtable api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      medium: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.airtable.com/v0/apporjVCnz1NflfHQ/content?api_key=keyHkyAlxZaDO9fJe')
+    .then((response) => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ medium: data.records });
+    }).catch(error => {
+      // Error
+    });
+  }
+
+  render() {
+    return (
+            <div>
+              <h1>/Product Management</h1>
+              {this.state.medium.map(productmanagement => <MediumCard {...productmanagement.fields} /> )}
+              {this.state.medium.map(productmanagement => <PodcastSection {...productmanagement.category} /> )}
+            </div>
+    );
+  }
 }
 
 export default App;
+
+const MediumCard = ({ name, description, url, notes }) => (
+  <div>
+    <div>
+      <h5>{name}</h5>
+      <p>{description}</p>
+      <p>{notes}</p>
+      <p>{url}</p>
+    </div>
+  </div>
+)
+
+const PodcastSection = ({ name, category, description, url, notes }) => (
+    <div>
+    <h1>{name}</h1>
+    </div>
+)
